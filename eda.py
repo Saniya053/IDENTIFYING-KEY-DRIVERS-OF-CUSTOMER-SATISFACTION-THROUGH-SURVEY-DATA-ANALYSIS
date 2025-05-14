99a2263728ef47865a3fe204fd741eb41348d3a8
saniya <saniyamuniyapillai798@gmail.com>
2025-05-14 15:35:16 +0530

    Add files via upload

---
+++ eda.py
@@ -0,0 +1,28 @@
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+
+sns.histplot(df['satisfaction_score'])
+
+plt.show()
+
+
+sns.countplot(x='product_quality', data=df)
+
+plt.show()
+
+
+sns.boxplot(x='product_quality', y='satisfaction_score', data=df)
+
+plt.show()
+
+
+sns.scatterplot(x='response_time', y='satisfaction_score', data=df)
+
+plt.show()
+
+
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+
+plt.show()
---
+++ sourcecode.py
@@ -0,0 +1,22 @@
+ import pandas as pd
+
+import numpy as np
+
+
+df = pd.read_csv('customer_satisfaction.csv')
+
+df = df.drop_duplicates()
+
+df.fillna(df.median(numeric_only=True), inplace=True)
+
+
+for col in df.select_dtypes(include='object').columns:
+
+    df[col].fillna(df[col].mode()[0], inplace=True)
+
+
+df['yes_no_column'] = df['yes_no_column'].map({'yes': 1, 'no': 0})
+
+numeric_cols = df.select_dtypes(include=np.number).columns
+df[numeric_cols] = df[numeric_cols].apply(lambda x: np.where(x > x.mean() + 3*x.std(), x.median(), x))
+ 
